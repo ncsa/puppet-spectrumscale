eabb801dedd16dc8be7556f0873fd69cf857b524
Andy Loftus <aloftus@illinois.edu>
2018-07-05 16:31:01 -0500

    Update firewall to allow list of ip sources or ranges

--- manifests/firewall.pp
+++ manifests/firewall.pp
@@ -1,23 +1,38 @@
 # Parameters: 
-#     allowed_cidr - String 
-#                  - gpfs ports will be opened in firewall to allow 
-#                    communication from this network space
-class gpfs::firewall( 
-    $allowed_cidr,
+#     allow_from - allow incoming traffic from these sources
+#                  on GPFS specific tcp ports
+class gpfs::firewall(
+    Array[String[1], 1] $allow_from,
 )
 {
-    # FIREWALL SETTINGS
-    firewall { '100 gpfs 1191':
-        dport  => 1191,
+    $common_parms = {
         proto  => tcp,
         action => accept,
-        source => $allowed_cidr,
     }
 
-    firewall { '100 gpfs 30K range':
-        dport  => '30000-30100',
-        proto  => tcp,
-        action => accept,
-        source => $allowed_cidr,
+    each( $allow_from ) |$ip_src| {
+        if '-' in $ip_src {
+            # Source looks like an ip-range (ie: IP1-IP2)
+            $key = 'src_range'
+        } else {
+            # Otherwise, assume ip_src is a valid "source"
+            # (ie: ip address or cidr)
+            $key = 'source'
+        }
+
+        # merged set of params for each firewall definition below
+        $fw_parms = merge( $common_parms, { $key => $ip_src } )
+
+        # Port 1191
+        firewall { '100 gpfs 1191':
+            dport => 1191,
+            *     => $fw_parms,
+        }
+
+        # Ports in the 30K range
+        firewall { '100 gpfs 30K range':
+            dport => '30000-30100',
+            *     => $fw_parms,
+        }
     }
 }
